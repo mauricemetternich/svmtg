343a8650f13e196d67cd7f42fbe35cbf59e63e07
Maurice Metternich <m.metternich@protonmail.ch>
2025-12-09 15:06:40 +0100

    alte DSB Referenz entfernt

--- include/Meldungen-angepasst-dsb.php
+++
@@ -1,5 +0,0 @@
-<?php 
-// Dieses Skript leitet die ehemalige DSB Seite auf die neue um.
-// Bitte löschen, wenn die neue URL von ********** eingetragen wurde.
-header("Location: http://svmtg.de/site/dsb.php");
-?>
